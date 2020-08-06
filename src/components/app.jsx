793b3c62e02b64d0489c5a6e9e4b892a3912e416
vy-hoang <57076819+vy-hoang@users.noreply.github.com>
2020-08-06 14:02:39 -0400

    practice react

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,35 +1,74 @@
-import React from "react";
+import React, { Component } from "react";
 import cars from "./data";
 import Details from "./details";
 import Style from "./style";
 
-function Items(cars){
-    return(
-        <div>
-            <Style />
-            <Details 
+// function Items(cars){
+//     return(
+//         <div>
+//             <Style />
+//             <Details 
             
-            color={cars.color}
-            key={cars.id}
-            type={cars.type}
-            registration={cars.registration}
-            capacity={cars.capacity}
+//             color={cars.color}
+//             key={cars.id}
+//             type={cars.type}
+//             registration={cars.registration}
+//             capacity={cars.capacity}
         
-            />
+//             />
 
-        </div>
+//         </div>
         
-    );
-}
+//     );
+// }
 
-function App(){
+// function App(){
     
-    return(
-        <div>
-            {cars.map(Items)}
-        </div>
-    );
+//     return(
+//         <div>
+//             <div>
+//                 <h1>You are currently (in/out)</h1>
+//             </div>
+//             {cars.map(Items)}
+//         </div>
+//     );
+// }
+// class App extends React.Component {
+//     constructor(){
+//         super()
+//         this.state = {
+//             isLoggedIn: false
+//         }
+//     }
+//     render(){
+//         let result 
+//         if (this.state.isLoggedIn === true){
+//             result = "in"
+//         }else{
+//             result = "out"
+//         }
+//         return(
+//             <div>
+//                <h1>You are currently {result}</h1>
+//             </div>
+//         );
+//         }
+// }
+class App extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            name: "Alice",
+            age: "12"
+        }
+    }
+    render(){
+        return(
+            <div>
+                <h1>{this.state.name}</h1>
+                <h3>{this.state.age} years old</h3>
+            </div>
+        )
+    }
 }
-
-
 export default App;
--- src/components/style.jsx
+++ src/components/style.jsx
@@ -1,12 +1,18 @@
 import React, {Component} from "react";
 
 class Style extends Component{
+    constructor() {
+        super()
+        this.state = {
+            answer: "yes"
+        }
+    }
     render(){
         const Decor = {
             color: "#1A5276"
         };
         return(
-            <h1 style = {Decor}>Car report</h1>
+            <h1 style = {Decor}>Car report {this.state.answer}</h1>
         )
     }
 }
