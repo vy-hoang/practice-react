b413f6f3a3d42411c501cb6aafba5e65f2ebbec8
vy-hoang <57076819+vy-hoang@users.noreply.github.com>
2020-08-07 14:31:52 -0400

    pracice the lifecycle method

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -122,6 +122,19 @@ class App extends Component{
         super()
         this.state = {date: new Date()};
     }
+    componentDidMount(){
+        this.timerID = setInterval(
+            () => this.tick(), 1000
+        );
+    }
+    componentWillMount(){
+        clearInterval(this.timerID);
+    }
+    tick(){
+        this.setState({
+            date: new Date()
+        });
+    }
     render() {
         return (
             <div>
